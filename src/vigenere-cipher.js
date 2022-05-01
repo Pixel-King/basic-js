3c4221438a0e25ed86c780f1889cd2d53a10aa78
Yaraslau Borodavkin <y.borodavkin2011@gmail.com>
2022-05-02 01:30:29 +0300

    vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,12 +19,93 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt(a,b) {
-    
+ class VigenereCipheringMachine {
+  constructor(type){
+    this.type = (type == undefined) ? true : false;;
+    this.alphovit ={
+      A: 0,
+      B: 1,
+      C: 2,
+      D: 3,
+      E: 4,
+      F: 5,
+      G: 6,
+      H: 7,
+      I: 8,
+      J: 9,
+      K: 10,
+      L: 11,
+      M: 12,
+      N: 13,
+      O: 14,
+      P: 15,
+      Q: 16,
+      R: 17,
+      S: 18,
+      T: 19,
+      U: 20,
+      V: 21,
+      W: 22,
+      X: 23,
+      Y: 24,
+      Z: 25
+    };
   }
-  decrypt(a,b) {
-
+  encrypt(str,key) {
+    if ((str == undefined) || (key == undefined)) {
+      throw new SyntaxError('Incorrect arguments!');
+    }
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let rez = "";
+    while(str.length>key.length){
+      key +=key;
+    }
+    for (let i =0,j =0;i<str.length;i++,j++){
+        let keykod = ((this.alphovit[str[i]] + this.alphovit[key[j]])>25)
+      ? this.alphovit[str[i]] + this.alphovit[key[j]] - 26 
+      : this.alphovit[str[i]] + this.alphovit[key[j]];
+      if (isNaN(keykod)){
+          rez += str[i];
+          j--;
+        
+      }else{
+        for (let ky in this.alphovit){
+          if (this.alphovit[ky] == keykod){
+            rez += ky;
+          }
+        }
+      }
+    }
+    return this.type?rez:rez.split('').reverse().join('');
+  }
+  decrypt(str,key) {
+    if ((str == undefined) || (key == undefined)) {
+      throw new SyntaxError('Incorrect arguments!');
+    }
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let rez = "";
+    while(str.length>key.length){
+      key +=key;
+    }
+    for (let i =0,j =0;i<str.length;i++,j++){
+      let keykod = ((this.alphovit[str[i]] - this.alphovit[key[j]])<0)
+    ? this.alphovit[str[i]] - this.alphovit[key[j]] + 26 
+    : this.alphovit[str[i]] - this.alphovit[key[j]];
+    if (isNaN(keykod)){
+        rez += str[i];
+        j--;
+      
+    }else{
+      for (let ky in this.alphovit){
+        if (this.alphovit[ky] == keykod){
+          rez += ky;
+        }
+      }
+    }
+  }
+  return this.type?rez:rez.split('').reverse().join('');
   }
 }
 
